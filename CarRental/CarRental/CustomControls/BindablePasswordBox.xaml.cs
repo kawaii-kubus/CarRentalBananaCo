5bdee3da105850c014673744b85fba59025e85b1
jciecielag <jakubciecielag@o2.pl>
2023-06-15 15:17:47 +0200

    Dodano działający panel hasła do LoginWindow

---
+++ CarRental/CarRental/CustomControls/BindablePasswordBox.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CarRental.CustomControls
+{
+    /// <summary>
+    /// Logika interakcji dla klasy BindablePasswordBox.xaml
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(SecureString), typeof(BindablePasswordBox));
+
+        public SecureString Password
+        {
+            get
+            {
+                return (SecureString)GetValue(PasswordProperty);
+            }
+            set
+            {
+                SetValue(PasswordProperty, value);
+            }
+        }
+
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+            txtPass.PasswordChanged += OnPasswordChanged;
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = txtPass.SecurePassword;
+        }
+    }
+}
