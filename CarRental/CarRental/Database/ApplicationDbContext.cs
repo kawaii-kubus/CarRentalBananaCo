d475fb441aa5e0f76176e07ac41a3d144142889e
jciecielag <jakubciecielag@o2.pl>
2023-06-18 14:59:00 +0200

    Dodano DbContext

---
+++ CarRental/CarRental/Database/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRental.Database.Tables;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CarRental.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        { }
+
+        public DbSet<ListaSamochodw> ListaSamochodow { get; set; }
+        public DbSet<Pracownicy> Pracownicy { get; set; }
+        public DbSet<Status> Status { get; set; }
+        public DbSet<ZamowieniaSamochodw> ZamowieniaSamochodow { get; set; }
+    }
+}
